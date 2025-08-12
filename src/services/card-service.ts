9eea5df7c961f939908c280e14e15cadc9eeb9f9
Michael Maniatis <mikejohnmaniatis@gmail.com>
2025-08-12 06:42:12 -0500

    Revert "fixing the touch issues"
    This reverts commit 108bca61ed731d255f5ab41b742f267944b23a9d.

--- src/services/card-service.ts
+++ src/services/card-service.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+//if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export class CardService {
   /**
